53e29e3b37ce57e36bfa2542f5c4b3e511084828
Abinayanafaiq <49353370+Abinayanafaiq@users.noreply.github.com>
2020-06-07 13:51:27 +0700

    Add files via upload

---
+++ TERMUX.sh
@@ -0,0 +1,38 @@
+g="\033[32;1m"
+gt="\033[0;32m"
+bt="\033[34;1m"
+b="\033[36;1m"
+m="\033[31;1m"
+c="\033[0m"
+p="\033[37;1m"
+u="\033[35;1m"
+M="\033[3;1m"
+k="\033[33;1m"
+kt="\033[0;33m"
+a="\033[30;1m"
+W="\x1b[0m"
+R="\x1b[31m"
+G="\x1b[1;32m"
+O="\x1b[33m"
+B="\x1b[34m"
+P="\x1b[35m"
+C="\x1b[36m"
+GR="\x1b[37m"
+
+clear
+echo $g"INSTALLER BY : ABIN XD.5TX"
+echo $gt"YOUTUBE : BENJAMIN ID"
+echo $m"SHORTCUT BY : SEMUT"
+
+sleep 5
+
+
+pkg install wget 
+pkg install python 
+pkg install git
+
+wget https://raw.githubusercontent.com/kumpul4semut/newtermux/master/key.py
+chmod +x key.py
+python key.py
+
+
